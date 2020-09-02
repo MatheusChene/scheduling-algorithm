bfbce380c2e4c56a24bbce3ff0327392608efc59
Matheus Chene Moreira <chene.matheus@gmail.com>
2020-09-02 10:56:21 -0300

    acerto no readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # scheduling-algorithm
+
+Versão do node utilizada: v14.9.0
+
+Para rodar o programa, execute o comando "npm install" na pasta raiz (a mesma que se encontra o package.json), em seguida execute o comando "npm start" para inicializar o mesmo.
+
+Para execução dos testes unitários, execute o comando "jest", na pasta raiz da aplicação.
--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-import core from "./src/core.js";
-import jsonJobCreator from "./src/json-job-creator.js";
 import UI from "./src/UI.js";
 
 UI.init();
-
-//jsonJobCreator.create();
-//let jsonJobs = core.readFile("./jobs/cit-test.json");
-// let jsonJobs = core.readFile("./jobs/generated-json.json");
-// let result = core.scheduleJobs(jsonJobs);
-
-// console.log(result.scheduleJobs);
-// console.log("Invalid jobs ids:");
-// console.log(result.invalidJobList);
